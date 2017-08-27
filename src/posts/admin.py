cd10eca14988c0f39962b18db862616dce421ec3
TejasBhitle <bhitletejas@gmail.com>
2017-08-27 19:47:47 +0530

    Post model added to admin app

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+class PostModelAdmin(admin.ModelAdmin) :
+
+    # to display columns
+    list_display = ["title", "content", "updated", "created"]
+
+    # to display links
+    list_display_links = ["updated"]
+
+    list_editable = ["title"]
+
+    list_filter = ["created", "updated"]
+
+    search_fields = ["title", "content"]
+
+
+# register model in admin
+admin.site.register(Post, PostModelAdmin)
