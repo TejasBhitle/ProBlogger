b383c6fad2f5c2c5c9a0ed46e192250c6170852c
TejasBhitle <bhitletejas@gmail.com>
2017-08-27 23:41:07 +0530

    Templates & basic queryset added

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
+
+from .models import Post
+
 # Create your views here.
 
 def post_list(request) :
-    return HttpResponse("<h1>Welcome</h1>")
+    queryset = Post.objects.all()
+    context = {
+        "title": "Post Lists",
+        "post_list": queryset
+    }
+    #return HttpResponse("<h1>Welcome</h1>")
+    return render(request, "post_list.html", context)
