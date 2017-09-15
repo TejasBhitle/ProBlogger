6591051b1909729f490b8174e7131b482cbef523
TejasBhitle <bhitletejas@gmail.com>
2017-09-15 14:16:04 +0530

    Preview div added in create/edit blog

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -4,7 +4,7 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
-    content = forms.CharField(widget=PagedownWidget)
+    content = forms.CharField(widget=PagedownWidget(show_preview=False))
     class Meta:
         model = Post
         fields = ["title", "content", "image"]
