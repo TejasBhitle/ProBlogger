730ca86975737e9ec614decfe908c7aa7ff26ded
TejasBhitle <bhitletejas@gmail.com>
2017-09-16 19:59:20 +0530

    Scripts issue fixed

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 
 from .views import (post_list, post_create,
                     post_detail, post_edit,
-                    post_delete)
+                    post_delete,my_post_list)
 
 urlpatterns = [
     url(r'^$', post_list, name='list'),
+    url(r'^my/$',my_post_list),
     url(r'^create/$', post_create),
     url(r'^(?P<post_id>\d+)/$', post_detail),
     url(r'^(?P<post_id>\d+)/edit$', post_edit),
--- src/posts/views.py
+++ src/posts/views.py
@@ -45,6 +45,37 @@ def post_list(request):
     return render(request, "post_list.html", context)
 
 
+def my_post_list(request):
+    queryset_list = Post.objects.filter(user=request.user).order_by("-created")
+
+    query = request.GET.get('q')
+    if query:
+        queryset_list = queryset_list.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(user__first_name__icontains=query) |
+            Q(user__last_name__icontains=query)
+        ).distinct()
+
+    paginator = Paginator(queryset_list, 4)  # Show 4 contacts per page
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+    context = {
+        "title": "Post Lists",
+        "post_list": queryset,
+        "page_request_var": page_request_var
+    }
+    return render(request, "post_list.html", context)
+
+
 @login_required(login_url='/user/login/')
 def post_create(request):
     # if not request.user.is_staff or not request.user.is_superuser:
